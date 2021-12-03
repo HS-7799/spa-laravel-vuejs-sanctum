a237b8cf8f6cb0a95bc377ef5128d74c852f56a8
hamza elharsi <hamza.elharsi@insa-cvl.fr>
2021-12-04 00:12:54 +0100

    add bootstrap and login, register forms

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,11 +1,25 @@
 <template>
-    <nav>
-          <ul>
-            <li>
-                <router-link to="/dashboard" >Dashboard</router-link>
-            </li>
-          </ul>
-      </nav>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SPA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <router-link to="/dashboard" class="nav-link" >dashboard</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link" >Login</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link" >Register</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -15,5 +29,10 @@ export default {
 </script>
 
 <style>
+
+.router-link-active
+{
+    color: aliceblue;
+}
 
 </style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,9 +3,13 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import Dashboard from '../components/Dashboard.vue'
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
 
 const routes = [
-    { path : '/dashboard', component : Dashboard  }
+    { path : '/dashboard', component : Dashboard  },
+    { path : '/login', component : Login  },
+    { path : '/register', component : Register  },
 ]
 
 
