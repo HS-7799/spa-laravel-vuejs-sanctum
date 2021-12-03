11118b23ec703cfe617932d00e7b05bc32fee2ad
hamza elharsi <hamza.elharsi@insa-cvl.fr>
2021-12-03 23:37:24 +0100

    set up vue js

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      greeting :
+      <h1>{{ greeting }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data()
+    {
+        return {
+            greeting :  'hello wolrd'
+        }
+    }
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+
+
+import App from './App.vue'
+const app = new Vue({
+    el: '#app',
+    render : h => h(App)
+});
